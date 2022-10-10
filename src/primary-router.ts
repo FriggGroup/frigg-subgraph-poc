9e670212d05d69bd9ff4639334477950be5c7c12
owieth <olivier.winkler@icloud.com>
2022-10-10 20:46:01 +0200

    feat(subgraph): add basic subgraph implementation #2

--- src/primary-router.ts
+++ src/primary-router.ts
@@ -1,65 +1,55 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  PrimaryRouter,
-  RoleAdminChanged,
-  RoleGranted,
-  RoleRevoked,
   SuccessfulExpiration,
   SuccessfulPurchase
 } from "../generated/PrimaryRouter/PrimaryRouter"
-import { ExampleEntity } from "../generated/schema"
+import {
+  Token
+} from '../generated/schema'
 
-export function handleRoleAdminChanged(event: RoleAdminChanged): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.role = event.params.role
-  entity.previousAdminRole = event.params.previousAdminRole
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.DEFAULT_ADMIN_ROLE(...)
-  // - contract.getRoleAdmin(...)
-  // - contract.hasRole(...)
-  // - contract.routerGater(...)
-  // - contract.supportsInterface(...)
-  // - contract.tokenData(...)
+export function handleSuccessfulPurchase(event: SuccessfulPurchase): void {
+  let token = new Token(event.params._friggTokenAddress.toHexString())
+  token.id = "0";
+  token.address = event.params._friggTokenAddress;
+  token.decimals = 18;
+  token.name = "ATT";
+  token.symbol = "ATT";
+  token.description = "ATT";
+  token.save()
 }
 
-export function handleRoleGranted(event: RoleGranted): void {}
-
-export function handleRoleRevoked(event: RoleRevoked): void {}
-
-export function handleSuccessfulExpiration(event: SuccessfulExpiration): void {}
-
-export function handleSuccessfulPurchase(event: SuccessfulPurchase): void {}
+export function handleSuccessfulExpiration(event: SuccessfulExpiration): void {
+  let token = new Token(event.params._friggTokenAddress.toHexString())
+  token.id = "0";
+  token.address = event.params._friggTokenAddress;
+  token.decimals = 18;
+  token.name = "ATT";
+  token.symbol = "ATT";
+  token.description = "ATT";
+  token.save()
+  /*
+  {
+    "blockNumber": 15582188,
+    "blockHash": "0xf32200f31ddb03521dab3527b77b4c5e493701d3534ca88349f61d05286dc03f",
+    "transactionIndex": 180,
+    "removed": false,
+    "address": "0x96418DF8B474e90E49183CC23fa41e4aD8B0ddbE",
+    "data": "0x00000000000000000000000090d53b872ce6421122b41a290acdd22a5ed931bd0000000000000000000000000000000000000000000000000000000002faf080",
+    "topics": [
+        "0xc676e61b1e79d2fb537a623c39c425429b06c3309e0f79e72ac1dccc697031b5",
+        "0x000000000000000000000000388f7bfc52bfa1d9d010309b3965d69177f5bbbe"
+    ],
+    "transactionHash": "0x63675661eaed4ec7330e3d5dcdf7a836040de06b417e6906d06c2df072f25acc",
+    "logIndex": 464,
+    "event": "SuccessfulPurchase",
+    "eventSignature": "SuccessfulPurchase(address,address,uint256)",
+    "args": [
+        "0x388F7bFc52bFa1d9D010309B3965d69177f5BbBe",
+        "0x90D53b872ce6421122B41a290aCdD22a5eD931bd",
+        {
+            "type": "BigNumber",
+            "hex": "0x02faf080"
+        }
+    ]
+}
+  */
+}
