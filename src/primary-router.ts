f2fd4bf72757127f416e7076e98feee51f1d4e98
owieth <olivier.winkler@icloud.com>
2022-10-13 15:29:40 +0200

    feat(subgraph): add frigg tokens to query #2

--- src/primary-router.ts
+++ src/primary-router.ts
@@ -1,55 +1,57 @@
 import {
   SuccessfulExpiration,
   SuccessfulPurchase
-} from "../generated/PrimaryRouter/PrimaryRouter"
+} from "../generated/PrimaryRouter/PrimaryRouter";
 import {
-  Token
-} from '../generated/schema'
+  Token, Transaction
+} from '../generated/schema';
+
+import { FriggToken } from '../generated/templates/FriggToken/FriggToken';
 
 export function handleSuccessfulPurchase(event: SuccessfulPurchase): void {
+  let transaction = new Transaction(event.transaction.hash.toHexString())
+  transaction.token = event.params._friggTokenAddress.toHexString();
+  transaction.amount = event.params._amount;
+  transaction.from = event.transaction.from;
+  transaction.to = event.transaction.to!;
+  transaction.transactionHash = event.transaction.hash;
+  transaction.timestamp = event.block.timestamp
+  transaction.tokenPhase = "BUY";
+  transaction.market = "PRIMARY";
+  transaction.save();
+
   let token = new Token(event.params._friggTokenAddress.toHexString())
-  token.id = "0";
+  let tokenContract = FriggToken.bind(event.params._friggTokenAddress);
+
   token.address = event.params._friggTokenAddress;
-  token.decimals = 18;
-  token.name = "ATT";
-  token.symbol = "ATT";
-  token.description = "ATT";
+  token.decimals = tokenContract.decimals();
+  token.name = tokenContract.name();
+  token.symbol = tokenContract.symbol();
   token.save()
 }
 
 export function handleSuccessfulExpiration(event: SuccessfulExpiration): void {
+  let transaction = new Transaction(event.transaction.hash.toHexString())
+
+  transaction.token = event.params._friggTokenAddress.toHexString();
+  transaction.amount = event.params._amount;
+  transaction.from = event.transaction.from;
+  transaction.to = event.transaction.to!;
+  transaction.transactionHash = event.transaction.hash;
+  transaction.timestamp = event.block.timestamp
+  transaction.tokenPhase = "SELL";
+  transaction.market = "PRIMARY";
+  transaction.save();
+
   let token = new Token(event.params._friggTokenAddress.toHexString())
-  token.id = "0";
-  token.address = event.params._friggTokenAddress;
-  token.decimals = 18;
-  token.name = "ATT";
-  token.symbol = "ATT";
-  token.description = "ATT";
-  token.save()
-  /*
-  {
-    "blockNumber": 15582188,
-    "blockHash": "0xf32200f31ddb03521dab3527b77b4c5e493701d3534ca88349f61d05286dc03f",
-    "transactionIndex": 180,
-    "removed": false,
-    "address": "0x96418DF8B474e90E49183CC23fa41e4aD8B0ddbE",
-    "data": "0x00000000000000000000000090d53b872ce6421122b41a290acdd22a5ed931bd0000000000000000000000000000000000000000000000000000000002faf080",
-    "topics": [
-        "0xc676e61b1e79d2fb537a623c39c425429b06c3309e0f79e72ac1dccc697031b5",
-        "0x000000000000000000000000388f7bfc52bfa1d9d010309b3965d69177f5bbbe"
-    ],
-    "transactionHash": "0x63675661eaed4ec7330e3d5dcdf7a836040de06b417e6906d06c2df072f25acc",
-    "logIndex": 464,
-    "event": "SuccessfulPurchase",
-    "eventSignature": "SuccessfulPurchase(address,address,uint256)",
-    "args": [
-        "0x388F7bFc52bFa1d9D010309B3965d69177f5BbBe",
-        "0x90D53b872ce6421122B41a290aCdD22a5eD931bd",
-        {
-            "type": "BigNumber",
-            "hex": "0x02faf080"
-        }
-    ]
-}
-  */
+  let tokenContract = FriggToken.bind(event.params._friggTokenAddress);
+
+  if (token) {
+    token.address = event.params._friggTokenAddress;
+    token.address = event.params._friggTokenAddress;
+    token.decimals = tokenContract.decimals();
+    token.name = tokenContract.name();
+    token.symbol = tokenContract.symbol();
+    token.save()
+  }
 }
